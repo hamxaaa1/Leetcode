d29bbec6ce8885666f315385205ff30364ecf9c6
Hamza Ashfaq <145859648+hamxaaa1@users.noreply.github.com>
2025-10-30 21:21:20 +0500

    Time: 100 ms (5.16%), Space: 62.1 MB (28.1%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,22 +4,26 @@
  * @return {number[]}
  */
 var topKFrequent = function (nums, k) {
-    let count = {}
+    let count = {};
     let output = []
     for (let i = 0; i < nums.length; i++) {
-        count[nums[i]] = (count[nums[i]] || 0) + 1
+        count[nums[i]] = (count[nums[i]] || 0) + 1;
+    }
+    for (let j = 0; j < k; j++) {
+        let maxCount = 0;
+        let maxKey = 1;
+
+        for (const key in  count) {
+            if (count[key] > maxCount) {
+                maxCount = count[key]
+                maxKey = key
+            }
+        }
+        output.push(Number(maxKey))
+        delete count[maxKey]
     }
 
-    let j = 0
-    for (const key in count) {
-        if (j < k) {
-            output.push(Number(key))
-            j++
-        } else if (j === k) {
-            break
-        }
-    }
+
     console.log(count)
-    console.log(output)
     return output
 };
