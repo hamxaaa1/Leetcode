b33d7e1b96a538d358369115d516673192e8e7b9
Hamza Ashfaq <145859648+hamxaaa1@users.noreply.github.com>
2025-11-18 21:29:01 +0500

    Time: 483 ms (5.97%), Space: 60.5 MB (95.84%) - LeetHub

---
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = Math.abs(nums[i] * nums[i])
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] > nums[j]) {
+                let temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+            }
+        }
+    }
+    return nums
+};
