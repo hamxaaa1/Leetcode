79beb1303eeca21aaa494f16c5bd63b6a34f06ab
Hamza Ashfaq <145859648+hamxaaa1@users.noreply.github.com>
2025-12-31 19:56:09 +0500

    Time: 11 ms (5.01%), Space: 58.7 MB (7.44%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number[]}
+ */
+var finalPrices = function (prices) {
+    for (let i = 0; i < prices.length; i++) {
+        for (let j = i + 1; j < prices.length; j++) {
+            if (prices[i] >= prices[j]) {
+                prices[i] = prices[i] - prices[j]
+                break
+            } else {
+                continue
+            }
+        }
+    }
+    console.log(prices)
+    return prices
+};
