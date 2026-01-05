7ea0adfe69aab900173f199127608c2e1cb26888
Hamza Ashfaq <145859648+hamxaaa1@users.noreply.github.com>
2026-01-05 19:41:59 +0500

    Time: 0 ms (100%), Space: 54.2 MB (53.21%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function (arr) {
+    let oddCount = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            oddCount++;
+        } else {
+            oddCount = 0;
+        }
+        if (oddCount >= 3) {
+            return true;
+        }
+    }
+    return false;
+};
