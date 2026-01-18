fe6f072ec3bad7519e7ed31770b462c07707a38d
Hamza Ashfaq <145859648+hamxaaa1@users.noreply.github.com>
2026-01-18 22:45:54 +0500

    Time: 4 ms (6.52%), Space: 55.2 MB (31.88%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfUnique = function (nums) {
+    let obj = {}
+    let sum = 0
+    for (let i = 0; i < nums.length; i++) {
+        obj[nums[i]] = (obj[nums[i]] || 0) + 1
+    }
+    console.log(obj)
+    for (const key in obj) {
+        if (obj[key] === 1) {
+            sum = sum + Number(key)
+        }
+    }
+    console.log(sum)
+    return sum
+};
