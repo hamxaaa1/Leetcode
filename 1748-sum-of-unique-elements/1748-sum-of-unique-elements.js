d7914a8f134e0d8696233a5b805ead5b45951494
Hamza Ashfaq <145859648+hamxaaa1@users.noreply.github.com>
2026-01-19 16:23:39 +0500

    Fix formatting and add semicolons in sumOfUnique

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var sumOfUnique = function (nums) {
-    let obj = {}
-    let sum = 0
+    let obj = {};
+    let sum = 0;
     for (let i = 0; i < nums.length; i++) {
-        obj[nums[i]] = (obj[nums[i]] || 0) + 1
+        obj[nums[i]] = (obj[nums[i]] || 0) + 1;
     }
     console.log(obj)
     for (const key in obj) {
         if (obj[key] === 1) {
-            sum = sum + Number(key)
+            sum = sum + Number(key);
         }
     }
-    console.log(sum)
-    return sum
+    console.log(sum);
+    return sum;
 };
