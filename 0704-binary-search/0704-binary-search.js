49b0d6ad3108a430b53385c14db1cfb646012ef2
Hamza Ashfaq <145859648+hamxaaa1@users.noreply.github.com>
2026-02-03 22:08:34 +0500

    Update 0704-binary-search.js

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -6,7 +6,7 @@
 var search = function(nums, target) {
     let start = 0;
     let end = nums.length - 1;
-    while(start <= end) {
+    while(start <= end) { 
         let mid = Math.floor((start + end) / 2);
         if (target < nums[mid]) {
             end = mid - 1;
