7d5e2af7a84a92e90796435bd2e06db1844e4044
Hamza Ashfaq <145859648+hamxaaa1@users.noreply.github.com>
2026-02-08 19:39:14 +0500

    Time: 3 ms (24.83%), Space: 56.4 MB (33.96%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if (x < 2) return x
+    let low = 0;
+    let high = x - 1;
+    while (low <= high) {
+        let mid = Math.floor((high + low) / 2)
+        if (mid * mid === x) return mid
+        if (mid * mid > x) {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+    return high
+};
