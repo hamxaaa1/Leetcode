d7011c30e84e9db0e79c606318e13518dd0bcb8f
Hamza Ashfaq <145859648+hamxaaa1@users.noreply.github.com>
2026-02-14 19:08:55 +0500

    Time: 4 ms (12.21%), Space: 57.5 MB (6.58%) - LeetHub

---
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {
+    let res = 0;
+    for (let num of nums) {
+        let str = String(num)
+        if(str.length %2 === 0) {
+            res++
+        }
+    }
+    return res
+};
