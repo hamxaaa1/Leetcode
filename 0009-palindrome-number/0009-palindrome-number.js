a847aaf5d43abf012757b7b07b6af1fcca3956b3
Hamza Ashfaq <145859648+hamxaaa1@users.noreply.github.com>
2026-02-16 19:31:08 +0500

    Update 0009-palindrome-number.js

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -7,7 +7,7 @@ var isPalindrome = function (x) {
     let left = 0; 
     let right = s.length - 1;
 
-    while (left < right) {
+    while (left < right) { 
         if (s[left] !== s[right]) return false; 
         left++;
         right--;
